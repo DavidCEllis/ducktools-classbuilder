d28e21f01525352e76a89850f5964018c8f48af6
David C Ellis <ducksual@gmail.com>
2024-06-05 09:01:44 +0100

    Test init, repr, eq, match_args, kw_only arguments passed correctly

--- tests/prefab/dynamic/test_subclass_implementation.py
+++ tests/prefab/dynamic/test_subclass_implementation.py
@@ -1,3 +1,7 @@
+import inspect
+
+import pytest
+
 from ducktools.classbuilder.prefab import Prefab, Attribute, SlotFields
 
 
@@ -46,6 +50,10 @@ class TestConstructionForms:
 
 
 class TestClassArguments:
+    """
+    Testing the non-default arguments given to a subclass
+    to make sure they are passed through
+    """
     def test_slots(self):
         class Ex(Prefab, slots=True):
             a: int
@@ -58,3 +66,115 @@ class TestClassArguments:
             b: int = 1
 
         assert "__slots__" not in vars(Ex)
+
+    def test_init(self):
+        class Ex(Prefab, init=True):
+            a: int
+            b: int = 1
+
+        ex1 = Ex(0)
+        ex2 = Ex(0, 1)
+        ex3 = Ex(a=0, b=1)
+
+        assert ex1 == ex2 == ex3
+
+        class ExNoInit(Prefab, init=False):
+            a: int
+            b: int = 1
+
+        assert "__init__" not in vars(ExNoInit)
+        assert "__prefab_init__" in vars(ExNoInit)
+
+        ex1 = ExNoInit()
+        ex1.__prefab_init__(0, 1)
+
+        assert (ex1.a, ex1.b) == (0, 1)
+
+    def test_repr(self):
+        class Ex(Prefab, repr=True):
+            a: int
+            b: int = 1
+
+        assert "__repr__" in vars(Ex)
+
+        class ExNoRepr(Prefab, repr=False):
+            a: int
+            b: int = 1
+
+        assert "__repr__" not in vars(ExNoRepr)
+
+    def test_eq(self):
+        class Ex(Prefab, eq=True):
+            a: int
+            b: int = 1
+
+        assert "__eq__" in vars(Ex)
+
+        class ExNoEq(Prefab, eq=False):
+            a: int
+            b: int = 1
+
+        assert "__eq__" not in vars(ExNoEq)
+
+    def test_iter(self):
+        class ExIter(Prefab, iter=True):
+            a: int
+            b: int = 1
+
+        assert "__iter__" in vars(ExIter)
+
+        class ExNoIter(Prefab, iter=False):
+            a: int
+            b: int = 1
+
+        assert "__iter__" not in vars(ExNoIter)
+
+        a, b = ExIter(0)
+        assert (a, b) == (0, 1)
+
+    def test_match_args(self):
+        class Ex(Prefab, match_args=True):
+            a: int
+            b: int = 1
+
+        assert "__match_args__" in vars(Ex)
+        assert Ex.__match_args__ == ("a", "b")
+
+        class ExNoMatchArgs(Prefab, match_args=False):
+            a: int
+            b: int = 1
+
+        assert "__match_args__" not in vars(ExNoMatchArgs)
+
+    def test_kw_only(self):
+        class Ex(Prefab, kw_only=False):
+            a: int
+            b: int = 1
+
+        ex1 = Ex(0)
+        ex2 = Ex(0, 1)
+        ex3 = Ex(a=0, b=1)
+        ex4 = Ex(0, b=1)
+
+        assert ex1 == ex2 == ex3 == ex4
+
+        sig = inspect.signature(Ex)
+        params = sig.parameters
+
+        assert params['a'].kind == inspect.Parameter.POSITIONAL_OR_KEYWORD
+        assert params['b'].kind == inspect.Parameter.POSITIONAL_OR_KEYWORD
+        assert params['b'].default == 1
+
+        class ExKWOnly(Prefab, kw_only=True):
+            a: int
+            b: int = 1
+
+        with pytest.raises(TypeError):
+            ex = ExKWOnly(0)
+
+        sig = inspect.signature(ExKWOnly)
+        params = sig.parameters
+
+        assert params['a'].kind == inspect.Parameter.KEYWORD_ONLY
+        assert params['b'].kind == inspect.Parameter.KEYWORD_ONLY
+        assert params['b'].default == 1
