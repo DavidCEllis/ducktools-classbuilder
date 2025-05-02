d99480c596fac008f9d217cee20831d3a043e9c7
David Ellis <ducksual@gmail.com>
2025-05-02 14:00:10 +0100

    Fix performance regression on current Python when getting 'attribute' declared classes without annotations.
    Previously this attempted to import `annotationlib` on 3.13 or earlier, which was slow. Now check for 3.14 first which is much faster.

--- src/ducktools/classbuilder/annotations.py
+++ src/ducktools/classbuilder/annotations.py
@@ -49,20 +49,19 @@ def get_ns_annotations(ns):
     if annotations is not None:
         annotations = annotations.copy()
     else:
-        # See if we're using PEP-649 annotations
-        # Guarding this with a try/except instead of a version check
-        # In case there's a change and PEP-649 somehow doesn't make 3.14
-        try:
-            from annotationlib import Format, call_annotate_function
-            get_annotate_function = _get_get_annotate_function()
-        except ImportError:
-            pass
-        else:
-            annotate = ns.get("__annotate__")  # Works in the early alphas
-            if not annotate:
-                annotate = get_annotate_function(ns)
-            if annotate:
-                annotations = call_annotate_function(annotate, format=Format.FORWARDREF)
+        if sys.version_info >= (3, 14):
+            # See if we're using PEP-649 annotations
+            try:
+                from annotationlib import Format, call_annotate_function
+                get_annotate_function = _get_get_annotate_function()
+            except ImportError:
+                pass
+            else:
+                annotate = ns.get("__annotate__")  # Works in the early alphas
+                if not annotate:
+                    annotate = get_annotate_function(ns)
+                if annotate:
+                    annotations = call_annotate_function(annotate, format=Format.FORWARDREF)
 
     if annotations is None:
         annotations = {}
