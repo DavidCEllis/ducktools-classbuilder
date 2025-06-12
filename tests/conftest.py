acae17ae11f1ac514709882d984a7b4bee8815b6
David C Ellis <ducksual@gmail.com>
2025-06-12 17:32:19 +0100

    Increase range to cover Python 3.15

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,10 @@
 import sys
 
-collect_ignore = []
+collect_ignore: list[str] = []
 
-if sys.version_info < (3, 15):
+if sys.version_info < (3, 16):
     minor_ver = sys.version_info.minor
 
     collect_ignore.extend(
-        f"py3{i+1}_tests" for i in range(minor_ver, 15)
+        f"py3{i+1}_tests" for i in range(minor_ver, 16)
     )
