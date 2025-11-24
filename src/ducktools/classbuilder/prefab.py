6edf0503244c4a0fd14076bcec756db57f321ba2
David C Ellis <ducksual@gmail.com>
2025-11-24 10:57:30 +0000

    Fix a performance regression on import time with annotations

--- src/ducktools/classbuilder/prefab.py
+++ src/ducktools/classbuilder/prefab.py
@@ -273,7 +273,7 @@ hash_maker = MethodMaker("__hash__", hash_generator)
 
 
 # Updated field with additional attributes
-class Attribute(Field):
+class Attribute(Field, ignore_annotations=True):
     """
     Get an object to define a prefab attribute
 
@@ -290,8 +290,8 @@ class Attribute(Field):
     :param metadata: Additional non-construction related metadata
     :param type: Type of this attribute (for slotted classes)
     """
-    iter: bool = True
-    serialize: bool = True
+    iter: bool = Field(default=True)  # type: ignore
+    serialize: bool = Field(default=True)  # type: ignore
     metadata: dict = Field(default=FIELD_NOTHING, default_factory=dict)  # type: ignore
 
 
