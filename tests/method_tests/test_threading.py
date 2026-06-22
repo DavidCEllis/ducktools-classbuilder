e0e47e0d5a5d34c46e3b2e85c0b711d5740b9c75
David C Ellis <ducksual@gmail.com>
2026-06-22 16:00:21 +0100

    Test that the cache is correctly locked

--- tests/method_tests/test_threading.py
+++ tests/method_tests/test_threading.py
@@ -2,7 +2,17 @@ import time
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 from ducktools.classbuilder.prefab import prefab
-from ducktools.classbuilder.methods import class_init_generator, MethodMaker
+from ducktools.classbuilder.methods import (
+    MethodMaker,
+
+    class_init_generator,
+    class_eq_generator,
+    counter_to_class_generator,
+    get_compare_args,
+
+    _counter_eq_generator,
+    _SimpleCache,
+)
 
 
 def test_multithreaded_generator():
@@ -32,3 +42,45 @@ def test_multithreaded_generator():
     # Assert generation has only occured once as there is only
     # one unique function in results
     assert len(results) == 1
+
+
+def test_multithreaded_cache():
+    thread_count = 50
+
+    def slow_counter_eq_generator(argcount, *, funcname="__eq__"):
+        time.sleep(0.01)
+        return _counter_eq_generator(argcount, funcname=funcname)
+
+    maker = MethodMaker(
+        "__eq__",
+        class_eq_generator,
+        cached_generator=counter_to_class_generator(
+            slow_counter_eq_generator,
+            get_compare_args,
+        )
+    )
+
+    stats = maker.cached_generator.cache.stats
+
+    examples = []
+    for _ in range(thread_count):
+        @prefab(init=False, eq=False, repr=False)
+        class Example:
+            a: int
+            b: int
+
+        maker.attach(Example)
+        examples.append(Example)
+
+    with ThreadPoolExecutor() as pool:
+        futures = [
+            pool.submit(getattr, example, "__eq__")
+            for example in examples
+        ]
+        results = set()
+        for future in as_completed(futures):
+            results.add(future.result())
+
+    assert len(results) == thread_count  # All different classes
+    assert stats.hits == thread_count - 1
+    assert stats.misses == 1
